fb2546036a90cff7051060f79126ebca615b3260
Richard Zhang <yanandwy@163.com>
2022-01-06 18:37:08 +0800

    Using the URL alias to fetch the about page so that it can be edited dynamically

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # wordpress-wechat
 A WeChat Mini Program for WordPress.
 
-Just one step to start the app: change the first line in the file `/utils/util.js`. The base URL should be changed to your website domain URL of WordPress.
+Just one step to start the app if you have a WordPress website already deployed:
+
+Change the first line in the file `/utils/util.js`. The base URL should be changed to your website domain URL of WordPress.
 
 ```Javascript
 const baseUrl = 'https://your.domain.com';
--- pages/about/about.js
+++ pages/about/about.js
@@ -12,11 +12,17 @@ Page({
 
     loadAboutData: function () {
         wx.showLoading({ title: '加载中' });
-        HttpClient.get('/wp-json/wp/v2/pages/84?_fileds=content', null, (res) => {
+        HttpClient.get('/wp-json/wp/v2/pages?_fields=content&slug=about', null, (res) => {
             wx.hideLoading();
 
-            let pageInfo = res.data;
-            this.setData({ pageModel: pageInfo });
+            let pageList = res.data;
+            if (pageList.length > 0) {
+                this.setData({ pageModel: pageList[0] });
+            } else {
+                this.setData({
+                    'pageModel.content.rendered': '<p>未找到关于页面！</p><p>请在 WordPress 后台设置关于页面，并把 URL 别名填写为 "about".</p>'
+                });
+            }
         });
     }
 })
